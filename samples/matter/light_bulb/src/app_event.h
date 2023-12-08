9a8c4538b3e91cc5b9b3cd91038a4e2c31662415
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2023-12-08 16:08:57 +0100

    samples: matter: Aligned Matter Light Bulb to new events
    Aligned Matter Light Bulb sample to the new version of events,
    buttons and LEDs.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- samples/matter/light_bulb/src/app_event.h
+++ samples/matter/light_bulb/src/app_event.h
@@ -8,42 +8,17 @@
 
 #include <cstdint>
 
-#include "event_types.h"
-
-class LEDWidget;
-
-enum class AppEventType : uint8_t {
-	None = 0,
-	Button,
-	ButtonPushed,
-	ButtonReleased,
-	Timer,
-	UpdateLedState,
-	Lighting,
-	IdentifyStart,
-	IdentifyStop,
-};
-
-enum class FunctionEvent : uint8_t { NoneSelected = 0, SoftwareUpdate = 0, FactoryReset, AdvertisingStart };
-
+enum class AppEventType : uint8_t { None = 0, Lighting, Button };
 struct AppEvent {
+	AppEvent(AppEventType type) { mType = type; }
+	AppEvent() = default;
+
 	union {
-		struct {
-			uint8_t PinNo;
-			uint8_t Action;
-		} ButtonEvent;
-		struct {
-			void *Context;
-		} TimerEvent;
 		struct {
 			uint8_t Action;
 			int32_t Actor;
 		} LightingEvent;
-		struct {
-			LEDWidget *LedWidget;
-		} UpdateLedStateEvent;
 	};
 
-	AppEventType Type{ AppEventType::None };
-	EventHandler Handler;
+	AppEventType mType{ AppEventType::None };
 };
