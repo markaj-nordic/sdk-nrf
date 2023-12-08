d7cb2318ad364c39f474ee48e26e50c2e7cfac7d
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2023-12-08 16:45:13 +0100

    samples: matter: Aligned Matter Template to new events Aligned Matter Template sample to the new version of events, buttons and LEDs.
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- samples/matter/template/src/app_config.h
+++ samples/matter/template/src/app_config.h
@@ -7,16 +7,3 @@
 #pragma once
 
 /* ---- Template App Config ---- */
-
-#include "board_util.h"
-
-#define FUNCTION_BUTTON DK_BTN1
-#define FUNCTION_BUTTON_MASK DK_BTN1_MSK
-
-#define SYSTEM_STATE_LED DK_LED1
-#define FACTORY_RESET_SIGNAL_LED DK_LED2
-
-#if NUMBER_OF_LEDS == 4
-#define FACTORY_RESET_SIGNAL_LED1 DK_LED3
-#define FACTORY_RESET_SIGNAL_LED2 DK_LED4
-#endif
